eb92dabc43893e2378c1ea8ee15ab890653fd88d
avearmin <arminradoncic1@gmail.com>
2024-05-12 00:50:52 -0400

    Add del command

---
+++ cmd/del.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/avearmin/shelly/internal/cmdstore"
+	"github.com/avearmin/shelly/internal/configstore"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func init() {
+	rootCmd.AddCommand(delCmd)
+}
+
+var delCmd = &cobra.Command{
+	Use:   "del",
+	Short: "Delete an alias and shell command.",
+	Long:  "Delete an alias and shell command that is managed by shelly.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if !configstore.Exists() {
+			fmt.Fprintln(os.Stderr, "shelly config doesn't exist. Please run 'shelly init'")
+			os.Exit(1)
+		}
+
+		config, err := configstore.Load()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		cmds, err := cmdstore.Load(config.CmdsPath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		delete(cmds, args[0])
+
+		if err := cmdstore.Save(config.CmdsPath, cmds); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+	},
+}
